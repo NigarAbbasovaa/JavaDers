5ddb9e6934f558fe7d4c02ae13ae3d031b816c1e
Nigar <nigarabbasova2717@gmail.com>
2026-09-23 22:53:19 +0400

    Task2

--- src/Task2.java
+++ src/Task2.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Task2 {
         public static void main(String[] args) {
-            System.out.println("xos gelmisen ay qudam");
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println("Cəm:"+(a+b));
+            System.out.println("Fərq:"+(a-b));
+            System.out.println("Hasil:"+(a*b));
+            System.out.println("Bölmə:"+(a/b));
+            System.out.println("Qalıq:"+(a%b));
+
         }
 }
